4918101856988655d99be7fb06d47537e429ebe4
drbackend <bardia.backend@gmail.com>
2020-07-03 15:06:20 +0430

    grpc-Friday-Jul-03-2020-15-06-20

--- pull.sh
+++ pull.sh
@@ -1,11 +1,4 @@
-
 git add .
-
 dt=$(date '+%A-%b-%d-%Y-%H-%M-%S');
-
 git commit -m "grpc-$dt"
-
-#git push origin
-
 git push --set-upstream origin master
-
--- push.sh
+++ push.sh
@@ -1,23 +1,12 @@
-
 echo -n "Enter url git project > "
-
 read url
-
 echo -n "Please select a name for commit > "
-
 read namecommit
-
 git init
-
 git add .
-
 git commit -m "$namecommit"
-
 git remote add origin $url
-
 git push origin master
-
 #if error
 # git pull --rebase origin master
 # git push origin master
-
